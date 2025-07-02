b0ca43a3c5f3be864784ee5aaa93fd0ff7565e07
Josephat Julius M <josephatjulius24@gmail.com>
2025-07-02 13:42:35 +0300

    Fix route issue on page reload using hashtag

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import Aura from '@primeng/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -9,16 +9,17 @@ import HRHPreset from '../hrh-presets';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     provideAnimationsAsync(),
     providePrimeNG({
-    theme: {
+      theme: {
         preset: HRHPreset,
         options: {
-            darkModeSelector: false || 'none'
-        }
-    }
-})
-  ]
+          darkModeSelector: false || 'none',
+        },
+      },
+    }),
+  ],
 };
